7a999ea7e427584dd2f4052e55c3d9d9f7da239d
KuzzminaKarina <136954821+KuzzminaKarina@users.noreply.github.com>
2023-08-02 09:57:23 +0300

    Add files via upload

--- RequestsTest/tests/test_pokemon.py
+++ RequestsTest/tests/test_pokemon.py
@@ -5,13 +5,18 @@ token = '56fad1c7010e42bd4511fd6706453bb6'
 host = 'https://api.pokemonbattle.me:9104'
 
 def test_status_code():
-    response = requests.get(f'{host}//trainers', params = {'trainer_id' : 1903})
+    response = requests.get(f'{host}/trainers', params = {'trainer_id' : 1903})
     assert response.status_code == 200
 
-def test_name_in_answer(): 
-    response = requests.get(f'{host}//trainers', params = {'trainer_id' : 1903})
+def test_part_of_answer(): 
+    response = requests.get(f'{host}/trainers', params = {'trainer_id' : 1903})
     assert response.json()['trainer_name'] == 'MAsh' 
     assert response.json()['city'] == 'Tokkyo' 
 
+@pytest.mark.parametrize('key, value', [('name', 'Мэрри'), 
+                                        ('trainer_id', '1903'), 
+                                        ('attack', '1.0')])
 
-    
+def test_parts_of_answer(key, value):
+    response = requests.get(f'{host}/pokemons', params = {'trainer_id' : 1903})
+    assert response.json()[0][key] == value
